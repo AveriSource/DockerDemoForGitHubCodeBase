160c4360f32f0a5a6a8eb8d93dd436bca2e6aa5a
amandeepqss1 <amandeep.singh@qsstechnosoft.com>
2025-02-28 15:19:55 +0530

    Commit code to resolve permission issue on github container

--- start-xvfb.sh
+++ start-xvfb.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Start Xvfb
 Xvfb :99 -screen 0 1024x768x16 &
-export DISPLAY=:99
+
+# Wait for Xvfb to be ready
+sleep 1
+
+# Start VNC server
+x11vnc -display :99 -forever -nopw -shared &
+
+# Execute the command passed to the script
 exec "$@" 
